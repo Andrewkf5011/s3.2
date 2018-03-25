c965a701d0e15e72c2aa3fd68dc4174043869c8f
AndrewBiscuit <andrewalford1@outlook.com>
2018-03-25 15:51:49 +0100

    Solution

--- src/main.cpp
+++ src/main.cpp
@@ -3,24 +3,29 @@
 
 AssignmentBoard board;
 
-//PwmOut red(D9);
+Potentiometer redController(board.SHIELD_LEFT_POTENTIOMETER);
+Potentiometer greenController(board.SHIELD_RIGHT_POTENTIOMETER);
 PWM_LED red(board.SHIELD_RED_LED);
+PWM_LED green(board.SHIELD_GREEN_LED);
 
 int main()
 {
   //Set the PWM period in milliseconds (100Hz, which is suitably fast).
   red.getPWM().period_ms(10);
+  green.getPWM().period_ms(10);
+
+  float redBrightness = 1;
+  float greenBrightness = 1;
 
   while(1)
   {
-    /* ramp up from 0 to 100% */
-    float duty;
+    redBrightness = redController.read();
+    greenBrightness = greenController.read();
 
-    for( duty=0 ; duty<4.6 ; duty+=0.23 )
-    {
-      float brightness = exp(duty)/100;
-      red.on(brightness);
-      wait_ms(100);
-    }
+    red.on(redBrightness);
+    green.on(greenBrightness);
+
+    printf("Left dial %f \n",  redController.read() );
+    printf("Right dial %f \n", greenController.read() );
   }
 }
